f888a47bd18c043ace72149a6f028fba6b458610
SavsaniRaj <savsaniraj1155@gmail.com>
2025-12-25 11:18:13 +0530

    Added first 5 Java programs

---
+++ EvenOrOdd.java
@@ -0,0 +1,11 @@
+class EvenOrOdd{
+    public static void main(String[]args){
+        int a = 1;
+        if(a % 2 ==0){
+            System.out.println("number is even ");
+
+        }else{
+            System.out.println("number is odd");
+        }
+    }
+}
---
+++ LargestNumber.java
@@ -0,0 +1,7 @@
+class LargestNumber{
+    public static void main(String[]args){
+        int a=320;
+        int b=30;
+        System.out.println(a>b?a:b);
+    }
+}
